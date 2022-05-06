e73142e8c590642bf9155a83f42e70334637f0f4
jisu <jisu3316@naver.com>
2022-05-06 17:52:18 +0900

    jpa study

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name= "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") //나는 주인에의해 만들어진 거울이다. 읽기전용
+    private List<Order> orders = new ArrayList<>();
+
+}
