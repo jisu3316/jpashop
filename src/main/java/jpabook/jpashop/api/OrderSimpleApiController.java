8f41722ffb25b5d19df724cb37914a6fe80dc93e
Jisu Kim <kimjisu3316@naver.com>
2022-06-14 22:54:51 +0900

    jpa study

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,6 +5,7 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.OrderSimpleQueryDto;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -61,6 +62,8 @@ public class OrderSimpleApiController {
                 .collect(Collectors.toList());
         return result;
     }
+
+
     @GetMapping("/api/v3/simple-orders")
     public List<SimpleOrderDto> ordersV3() {
         List<Order> orders =  orderRepository.findAllWithMemberDelivery();
@@ -69,6 +72,10 @@ public class OrderSimpleApiController {
                 .collect(Collectors.toList());
         return result;
 
+    }
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> orderV4() {
+        return orderRepository.findOrderDtos();
     }
 
     @Data
