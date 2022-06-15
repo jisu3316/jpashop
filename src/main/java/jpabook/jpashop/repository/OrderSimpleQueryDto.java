8334858d56c9aa4129ae6fa3bad7295858e0b99b
jisu3316 <kimjisu3316@naver.com>
2022-06-15 21:21:33 +0900

    jpa study

---
+++ src/main/java/jpabook/jpashop/repository/OrderSimpleQueryDto.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OrderSimpleQueryDto {
+    private Long orderID;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private Address address;
+
+    public OrderSimpleQueryDto(Long orderID, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderID = orderID;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+    }
+
+}
